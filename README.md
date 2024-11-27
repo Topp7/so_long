8189fc6c3b74a2d40b10c86d2a8b3abd5b39a481
Soren Topp <stopp@2-A-5.42heilbronn.de>
2024-11-27 14:02:16 +0100

    added subject and Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+[What the game looks like](./assets/screenshot.png)
+
+You can move the Playermodel using WASD and Exit with ESC. The Exit is only available when all boxes are collected and the move need to finish are counted.
+
+Here is an Overview of the subject:
+
+[View the Subject](./en.subject.pdf)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: stopp <stopp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 15:45:01 by stopp             #+#    #+#             */
-/*   Updated: 2024/03/24 17:02:51 by stopp            ###   ########.fr       */
+/*   Updated: 2024/04/14 14:27:07 by stopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_data	*data;
-	int		i;
 
-	i = 0;
 	if (argc != 2)
 		return (int_error("Invalid Arguments\n"));
 	if (ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4], ".ber", 4) != 0)
